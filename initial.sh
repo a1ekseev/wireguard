5b723b75289ac0495cc5b8f86bf9ee9565eb071e
Кирилл Алексеев <a1ekseev@MacBook-Pro.local>
2020-06-13 17:22:42 +0300

    some fixes

--- initial.sh
+++ initial.sh
@@ -11,5 +11,8 @@ sysctl -w net.ipv6.conf.lo.disable_ipv6=1 && \
 curl -Lo wireguard.zip https://github.com/a1ekseev/wireguard/archive/master.zip
 
 unzip -j wireguard.zip -d /etc/wireguard/
+rm -f wireguard.zip
+chmod +x /etc/wireguard/add_client.sh /etc/wireguard/install_server.sh /etc/wireguard/remove_client.sh
+umask 077 /etc/wireguard/add_client.sh /etc/wireguard/install_server.sh /etc/wireguard/remove_client.sh
 
 ./etc/wireguard/install_server.sh
