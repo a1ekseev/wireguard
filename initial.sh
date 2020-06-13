9002528c944fc2b649dd64bc5739191b2031644d
Кирилл Алексеев <a1ekseev@MacBook-Pro.local>
2020-06-13 18:06:36 +0300

    some changes

--- initial.sh
+++ initial.sh
@@ -6,7 +6,7 @@ apt install unzip resolvconf curl qrencode ufw -y && \
 sysctl -w net.ipv4.ip_forward=1 && \
 sysctl -w net.ipv6.conf.all.disable_ipv6=1 && \
 sysctl -w net.ipv6.conf.default.disable_ipv6=1 && \
-sysctl -w net.ipv6.conf.lo.disable_ipv6=1 && \
+sysctl -w net.ipv6.conf.lo.disable_ipv6=1
 
 curl -Lo wireguard.zip https://github.com/a1ekseev/wireguard/archive/master.zip
 
